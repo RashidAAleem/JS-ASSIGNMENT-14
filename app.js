5a4e89b10225dd90e0eec1dd2014688b4e8c9e21
Rana Rashid <124416215+RashidAAleem@users.noreply.github.com>
2024-10-18 06:30:33 +0000

    update

---
+++ app.js
@@ -0,0 +1,79 @@
+//q1
+
+// function currDate(){
+// var today = new Date
+//     document.write(today)
+// }
+
+// currDate()
+
+//q2
+// function greet(){
+// var firstName = prompt("Enter Your First Name")
+// var lastName = prompt("Enter Your Last Name")
+// alert("Hello!.... " + firstName+ " " + lastName)
+
+// }
+
+// greet();
+
+
+//q3
+
+// function addNum(){
+
+//  var firstNum = +prompt("Please enter first number")
+//  var secondNum = +prompt("Please enter second number")
+//  return xyz = firstNum + secondNum   
+// }
+
+// addNum()
+// var a = xyz
+// document.write(a)
+
+//q4
+
+// function calc() {
+//     var firstNum = +prompt("Please enter first number");
+//     var secondNum = +prompt("Please enter second number");
+//     var operation = prompt("Please enter operation you want to perform (+, -, *, or /)");
+
+//     var result;
+    
+//     switch (operation) {
+//         case '+':
+//             result = firstNum + secondNum;
+//             break;
+//         case '-':
+//             result = firstNum - secondNum;
+//             break;
+//         case '*':
+//             result = firstNum * secondNum;
+//             break;
+//         case '/':
+//             result = firstNum / secondNum;
+//             break;
+//         default:
+//             result = "Calculator";
+//     }
+
+//     alert("The result is: " + result);
+// }
+
+// calc();
+
+
+
+//q5
+
+function square(number) {
+    
+    var result = number * number
+    document.write(result)
+}
+
+var number = +prompt("Enter a Number")
+square(number)
+
+//q6
+
